89c6375153859b7df931773912cf8afc3129ffac
chanhl22 <lch9502@naver.com>
2025-04-03 21:41:20 +0900

    feat: 상품 / 상위 상품 조회 API 수정 및 Swagger 문서화

--- src/main/kotlin/kr/hhplus/be/server/product/ProductController.kt
+++ src/main/kotlin/kr/hhplus/be/server/product/ProductController.kt
@@ -1,27 +1,27 @@
 package kr.hhplus.be.server.product
 
 import kr.hhplus.be.server.product.response.ProductResponse
-import kr.hhplus.be.server.product.response.TopProductResponse
+import kr.hhplus.be.server.product.response.TopSellingProductResponse
+import kr.hhplus.be.server.swagger.ProductApi
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ProductController {
+class ProductController : ProductApi {
 
     @GetMapping("/product/{id}")
-    fun findProduct(
+    override fun find(
         @PathVariable id: Long,
     ): ProductResponse {
-        return ProductResponse(1L, "상품 A", 10000L, 10)
+        return ProductResponse(1L, "무선 블루투스 이어폰", 129000, 25, "고음질 무선 블루투스 이어폰. 최대 20시간 사용 가능.")
     }
 
-    @GetMapping("/product/top")
-    fun findTopSellingProducts(): List<TopProductResponse> {
+    @GetMapping("/products/top")
+    override fun findTopSellingProducts(): List<TopSellingProductResponse> {
         return listOf(
-            TopProductResponse(1L, "노트북", 1500000, 10),
-            TopProductResponse(1L, "스마트폰", 800000, 5),
-            TopProductResponse(1L, "무선 이어폰", 200000, 1)
+            TopSellingProductResponse(1, 10L, "무선 블루투스 이어폰", 129000, 350, 25),
+            TopSellingProductResponse(2, 5L, "게이밍 키보드", 89000, 280, 50),
         )
     }
 
--- src/main/kotlin/kr/hhplus/be/server/product/response/ProductResponse.kt
+++ src/main/kotlin/kr/hhplus/be/server/product/response/ProductResponse.kt
@@ -1,8 +1,9 @@
 package kr.hhplus.be.server.product.response
 
 data class ProductResponse(
-    val id: Long,
+    val productId: Long,
     val name: String,
     val price: Long,
-    val stock: Int
+    val stock: Int,
+    val description: String
 )
