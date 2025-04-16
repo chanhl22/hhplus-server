1d48a788a76c44d268e571e50c7f483ae5fca6a8
chanhl22 <lch9502@naver.com>
2025-04-16 18:58:13 +0900

    feat: User Infrastructure 작성 및 UserService 통합테스트

--- src/main/kotlin/kr/hhplus/be/server/domain/user/User.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/user/User.kt
@@ -1,7 +1,23 @@
 package kr.hhplus.be.server.domain.user
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
 class User(
-    val id: Long,
     val name: String,
     val pointId: Long,
-)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) {
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+        }
+    }
+
+}
