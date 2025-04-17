1708000ebf34d303f7f98640eb4db529eed2c9dd
chanhl22 <lch9502@naver.com>
2025-04-17 22:40:10 +0900

    fix: 테스트 수정

--- src/main/kotlin/kr/hhplus/be/server/domain/product/ProductStatistics.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/product/ProductStatistics.kt
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
 @Entity
+@Table(name = "product_statistics")
 class ProductStatistics(
     val productId: Long,
     val totalSales: Int,
