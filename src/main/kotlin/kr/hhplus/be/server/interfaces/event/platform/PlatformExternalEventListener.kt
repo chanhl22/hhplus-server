1bb9a8ab8b412644b0c9c797126b48e20615e51e
chanhl22 <lch9502@naver.com>
2025-05-29 01:09:51 +0900

    feat: 커밋 추가

--- src/main/kotlin/kr/hhplus/be/server/interfaces/event/platform/PlatformExternalEventListener.kt
+++ src/main/kotlin/kr/hhplus/be/server/interfaces/event/platform/PlatformExternalEventListener.kt
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.interfaces.event.platform
 import kr.hhplus.be.server.domain.order.OrderEvent
 import kr.hhplus.be.server.domain.platform.PlatformSendService
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,9 +12,10 @@ class PlatformExternalEventListener(
 ) {
 
     @KafkaListener(topics = ["order_completed"], groupId = "platform-service")
-    fun handle(event: OrderEvent.Completed) {
+    fun handle(event: OrderEvent.Completed, ack: Acknowledgment) {
         val command = PlatformExternalEventMapper.toCommand(event)
         platformSendService.send(command)
+        ack.acknowledge()
     }
 
 }
